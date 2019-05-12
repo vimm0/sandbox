510348c30226d7d3a54765935f32e380039e7cce
vimm0 <vimmrana0@gmail.com>
2019-05-12 11:04:27 +0545

    update defaults via setting and utils

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+from ..defaults import *  # noqa
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '$fpf@7=@k8!3x-zl2hfw!qvha*c7zrk#v@tb3wexntaty=75dq'
---
+++ sandbox/utils.py
@@ -0,0 +1,54 @@
+from django.apps import apps
+from django.apps.config import MODELS_MODULE_NAME
+from django.core.exceptions import AppRegistryNotReady
+from importlib import import_module
+
+
+def get_model(app_label, model_name):
+    """
+    Fetches a Django model using the app registry.
+
+    This doesn't require that an app with the given app label exists,
+    which makes it safe to call when the registry is being populated.
+    All other methods to access models might raise an exception about the
+    registry not being ready yet.
+    Raises LookupError if model isn't found.
+    USAGE:
+        admin.site.register(get_model('address', 'useraddress'), UserAddressAdmin)
+    """
+    try:
+        return apps.get_model(app_label, model_name)
+    except AppRegistryNotReady:
+        if apps.apps_ready and not apps.models_ready:
+            # If this function is called while `apps.populate()` is
+            # loading models, ensure that the module that defines the
+            # target model has been imported and try looking the model up
+            # in the app registry. This effectively emulates
+            # `from path.to.app.models import Model` where we use
+            # `Model = get_model('app', 'Model')` instead.
+            app_config = apps.get_app_config(app_label)
+            # `app_config.import_models()` cannot be used here because it
+            # would interfere with `apps.populate()`.
+            import_module('%s.%s' % (app_config.name, MODELS_MODULE_NAME))
+            # In order to account for case-insensitivity of model_name,
+            # look up the model through a private API of the app registry.
+            return apps.get_registered_model(app_label, model_name)
+        else:
+            # This must be a different case (e.g. the model really doesn't
+            # exist). We just re-raise the exception.
+            raise
+
+
+def is_model_registered(app_label, model_name):
+    """
+    Checks whether a given model is registered. This is used to only
+    register models if they aren't overridden by a forked app.
+    USAGE:
+        if not is_model_registered('address', 'UserAddress')
+    """
+    try:
+        apps.get_registered_model(app_label, model_name)
+    except LookupError:
+        return False
+    else:
+        return True
